359125ce817b7e8fe50e293378a127c0d4a825d6
Mai Ngoc Gia Bao <maingocgiabao@gmail.com>
2022-04-01 09:44:00 -0700

    Added flickity, some js

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,59 @@
+function flickity() {
+  $('.hero__wrapper').flickity({
+    // options
+    cellAlign: 'left',
+    wrapAround: true,
+    contain: true,
+    autoPlay: 4000,
+    pauseAutoPlayOnHover: false,
+    lazyLoad: 2,
+    selectedAttraction: 0.01,
+    friction: 0.15
+  });
+}
 
 
-console.log('Hi');
+
+function ServiceCard() {
+  const Icon = $(".Icon");
+
+  Icon.each(function (index, element) {
+    $(element).on("click", function (e) {
+      $(".info").css({ "display": "none" })
+      $(".info").eq(index).css({
+        "display": "block"
+      })
+
+      $(".card p").removeClass("active")
+      $(".card p").eq(index).addClass("active");
+
+
+    })
+  })
+}
+
+
+
+function scrollToWelcome() {
+  const header = document.querySelector('header').offsetHeight;
+  const button = document.querySelector('.hero__button a');
+  const className = button.getAttribute('href').replace('#', '.');
+  const welcome = document.querySelector(className);
+  $('.hero__button a').on("click", (e) => {
+    e.preventDefault();
+
+    console.log("scrolled")
+    window.scrollTo({
+      top: welcome.offsetTop - header,
+      behavior: "smooth"
+    });
+
+  })
+}
+
+$(function () {
+
+  flickity();
+  ServiceCard();
+  scrollToWelcome();
+});
